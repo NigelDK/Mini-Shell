5dfc8f182983b5058ca5ff928d6c9c5b234a1db8
Nigel De Koninck <nigel.de.koninck@gmail.com>
2021-03-31 13:48:29 +0200

    added lexer

--- srcs/main.c
+++ srcs/main.c
@@ -1,31 +1,47 @@
 #include "../includes/minishell.h"
 
-void	infinity_loop(t_v *v)
+void	infinity_loop(t_v *v, char *line)
 {
-	char    *line;
-	int i;
-	int ret;
-	int cd;
-	char **words;
+	int		i;
+	int		cd;
+	char	**words;
 
 	cd = 0;
+	if (!(words = ft_split(line, ' ')))
+		ft_error_split(&line, &words);
+	if (ft_strcmp_2(words[0], "echo", 1) == 0)
+		ft_echo(words, line, v);
+	else if (ft_strcmp_2(words[0], "cd", 1) == 0)
+		cd = ft_cd(words, cd);
+	else if (ft_strcmp_2(words[0], "pwd", 1) == 0)
+		ft_pwd();
+	else if (ft_strcmp_2(words[0], "export", 1) == 0)
+		ft_export(words, v);
+	else if (ft_strcmp_2(words[0], "env", 1) == 0)
+		ft_env(words, v);
+	ft_print_prompt(cd);
+	i = -1;
+	while (words[++i])
+		free(words[i]);
+	free(words);
+}
+
+void	lexer(t_v *v)
+{
+	char	*line;
+	char	**words;
+	int		ret;
+	int		i;
+	
 	while ((ret = get_next_line(0, &line)) > 0)
 	{
 		if (ret == -1)
 			ft_error();
-		if (!(words = ft_split(line, ' ')))
+		if (!(words = ft_split(line, ';')))
 			ft_error_split(&line, &words);
-		if (ft_strcmp_2(words[0], "echo", 1) == 0)
-			ft_echo(words, line, v);
-		else if (ft_strcmp_2(words[0], "cd", 1) == 0)
-			cd = ft_cd(words, cd);
-		else if (ft_strcmp_2(words[0], "pwd", 1) == 0)
-			ft_pwd();
-		else if (ft_strcmp_2(words[0], "export", 1) == 0)
-			ft_export(words, v);
-		else if (ft_strcmp_2(words[0], "env", 1) == 0)
-			ft_env(words, v);
-		ft_print_prompt(cd);
+		i = -1;
+		while (words[++i])
+			infinity_loop(v, words[i]);
 		i = -1;
 		while (words[++i])
 			free(words[i]);
@@ -59,6 +75,6 @@ int	main(int argc, char **argv, char **envp)
 	argv = NULL;
 	ft_print_prompt_2();
 	ft_envp(&v, envp);
-	infinity_loop(&v);
+	lexer(&v);
 	return (0);
 }
