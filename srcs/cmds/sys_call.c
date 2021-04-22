aa0ebee469bbd24fb0a8f5af9bb518d78d1a81ee
Mikko Numminen <minummin@shi-r1-p1.s19.be>
2021-04-22 17:30:21 +0200

    norminette

--- srcs/cmds/sys_call.c
+++ srcs/cmds/sys_call.c
@@ -6,32 +6,32 @@
 /*   By: nde-koni <nde-koni@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 18:22:33 by nde-koni          #+#    #+#             */
-/*   Updated: 2021/04/12 13:53:47 by nde-koni         ###   ########.fr       */
+/*   Updated: 2021/04/22 17:29:28 by minummin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sysexits.h>
 #include "../../includes/minishell.h"
 
 char	*path_variable(t_ls *data, int *j, t_v **v)
 {
-	struct stat info;
-	char **path;
-	char *temp;
-	char *cmd;
-	int i;
+	struct stat	info;
+	char		**path;
+	char		*temp;
+	char		*cmd;
+	int			i;
 
 	i = -1;
 	temp = ft_strjoin("/", data->words2[0]);
 	path = shell_split(getenv("PATH"), ':');
 	while (path[++i])
 	{
-		if (!(cmd = ft_strjoin2(path[i], temp)))
+		cmd = ft_strjoin2(path[i], temp);
+		if (!cmd)
 			ft_error_syscall(data, v, path, temp);
 		if (lstat(cmd, &info) == 0)
 		{
 			*j = 1;
-			break;
+			break ;
 		}
 		free(cmd);
 	}
@@ -40,52 +40,61 @@ char	*path_variable(t_ls *data, int *j, t_v **v)
 	return (cmd);
 }
 
+void	child_call_exit(t_ls *data, t_v **v, char *cmd, int j)
+{
+	if (j == 1)
+		free(cmd);
+	ft_error_data_v_child(data, v);
+	exit(127);
+}
+
+void	child_call(t_ls *data, t_v **v, char *cmd, int j)
+{
+	char	*temp;
+
+	if (execve(cmd, data->words2, data->envp) < 0)
+	{
+		if (data->words2[0][0] == '$')
+		{
+			if (data->words2[0][1]
+					== '?' && ft_isalpha(data->words2[0][2]) == 0)
+				printf("%s: %d\n", strerror(errno), data->statuscode);
+			while ((*v)->next)
+			{
+				temp = ft_strstr_reverse((*v)->str, "=");
+				if (ft_strcmp_2(data->words2[0], temp, 0) == 0)
+					printf("%s: %s\n",
+						ft_strstr_2((*v)->str, "="), strerror(errno));
+				free(temp);
+				*v = (*v)->next;
+			}
+		}
+		else
+			printf("zsh: command not found: %s\n", data->words2[0]);
+	}
+	child_call_exit(data, v, cmd, j);
+}
+
 void	sys_call(t_ls *data, t_v **v)
 {
 	pid_t	pid;
 	char	*cmd;
-	int j;
-	char	*temp;
-	int	wstatus;
+	int		j;
+	int		wstatus;
 
 	j = 0;
 	cmd = path_variable(data, &j, v);
-	if ((pid = fork()) < 0)
+	pid = fork();
+	if (pid < 0)
 	{
 		free(cmd);
 		ft_error_data_v_2(data, v);
 	}
 	if (pid == 0)
-	{
-		if (execve(cmd, data->words2, data->envp) < 0)
-		{
-			if (data->words2[0][0] == '$')
-			{
-				if (data->words2[0][1] == '?' && ft_isalpha(data->words2[0][2]) == 0)
-					printf("%s: %d\n", strerror(errno), data->statuscode);
-				while ((*v)->next)
-				{
-					temp = ft_strstr_reverse((*v)->str, "=");
-					if (ft_strcmp_2(data->words2[0], temp, 0) == 0)
-						printf("%s: %s\n", ft_strstr_2((*v)->str, "="), strerror(errno));
-					free(temp);
-					*v = (*v)->next;
-				}
-			}
-			else
-				printf("zsh: command not found: %s\n", data->words2[0]);
-		}
-		if (j == 1)
-			free(cmd);
-		ft_error_data_v_child(data, v);
-		exit (127);
-	}
-	else
-	{
-		if (j == 1)
-			free(cmd);
-		waitpid(pid, &wstatus, 0);
-		if (WIFEXITED(wstatus))
-			data->statuscode = WEXITSTATUS(wstatus);
-	}
+		child_call(data, v, cmd, j);
+	if (j == 1)
+		free(cmd);
+	waitpid(pid, &wstatus, 0);
+	if (WIFEXITED(wstatus))
+		data->statuscode = WEXITSTATUS(wstatus);
 }
--- srcs/cmds/unset.c
+++ srcs/cmds/unset.c
@@ -6,13 +6,13 @@
 /*   By: nde-koni <nde-koni@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 14:35:12 by minummin          #+#    #+#             */
-/*   Updated: 2021/04/08 20:58:58 by nde-koni         ###   ########.fr       */
+/*   Updated: 2021/04/22 17:07:56 by minummin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void    ft_memdel(char *ap)
+void	ft_memdel(char *ap)
 {
 	if (ap)
 	{
@@ -23,8 +23,8 @@ void    ft_memdel(char *ap)
 
 void	ft_unset2(char **words, t_v **v)
 {
-	char *temp;
-	t_v *t;
+	char	*temp;
+	t_v		*t;
 
 	temp = NULL;
 	if (!words[1])
@@ -45,37 +45,40 @@ void	ft_unset2(char **words, t_v **v)
 	}
 }
 
-
-void    ft_unset(char **words, t_v *v)
+void	ft_while_unset(char **words, t_v *v, char *temp)
 {
-	int i;
-	char *temp;
-	t_v *t;
+	int	i;
+	t_v	*t;
+
+	while (v->next && v->next->next)
+	{
+		i = 0;
+		while (words[++i])
+		{
+			if (v->next->str)
+			{
+				temp = ft_strstr_reverse(v->next->str, "=");
+				if (ft_strcmp_2(words[i], temp, 1) == 0)
+				{
+					i = 0;
+					t = v->next->next;
+					ft_lstdelone_2(v->next, ft_memdel);
+					v->next = t;
+				}
+				free(temp);
+			}
+		}
+		v = v->next;
+	}
+}
+
+void	ft_unset(char **words, t_v *v)
+{
+	char	*temp;
 
 	temp = NULL;
 	if (!words[1])
 		printf("unset: not enough arguments\n");
 	else
-	{
-		while (v->next && v->next->next)
-		{
-			i = 0;
-			while (words[++i])
-			{
-				if (v->next->str)
-				{
-					temp = ft_strstr_reverse(v->next->str, "=");
-					if (ft_strcmp_2(words[i], temp, 1) == 0)
-					{
-						i = 0;
-						t = v->next->next;
-						ft_lstdelone_2(v->next, ft_memdel);
-						v->next = t;
-					}
-					free(temp);
-				}
-			}
-			v = v->next;
-		}
-	}
+		ft_while_unset(words, v, temp);
 }
