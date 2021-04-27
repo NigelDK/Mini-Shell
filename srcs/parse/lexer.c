09f2dffea5daf6210194039346e08a826ea8bf1e
NigelDK <nigel.de.koninck@gmail.com>
2021-04-27 17:40:37 +0200

    ;

--- srcs/parse/lexer.c
+++ srcs/parse/lexer.c
@@ -6,7 +6,7 @@
 /*   By: nde-koni <nde-koni@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 10:36:18 by nde-koni          #+#    #+#             */
-/*   Updated: 2021/04/27 17:10:13 by nde-koni         ###   ########.fr       */
+/*   Updated: 2021/04/27 17:39:34 by nde-koni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,28 @@ void	lexer_2(t_v **v, t_ls *data, t_term t, char *tester)
 		return ;
 	if (t.errcode == 1)
 		data->statuscode = 1;
-	//              ret = -5; //tester1
+
 	if (data->line[0] && data->line[0] != '\n' && !syntax_error_check(data))
 	{
-		data->words = shell_split(data->line, ';'); // tester0
+		data->words = shell_split(data->line, ';');
 		if (!data->words)
-			//      if (!(data->words = shell_split(tester, ';'))) // tester1
+
 			ft_error_data_v(data, *v);
 		z = -1;
 		while (data->words[++z])
+		{
+			if (data->words[z][0] == '\n')
+				continue ;
 			if (!ft_pipe(*v, &data->words[z], data))
 				infinity_loop(v, data->words[z], data);
+		}
 		z = -1;
 		while (data->words[++z])
 			free(data->words[z]);
 		free(data->words);
 	}
 	if (g_sigint == 1)
-		ft_print_prompt(data, *v); // tester0
+		ft_print_prompt(data, *v);
 }
 
 void	lexer(t_v *v, t_ls *data, char *tester, t_term t)
@@ -46,11 +50,9 @@ void	lexer(t_v *v, t_ls *data, char *tester, t_term t)
 	int		in;
 
 	in = 1;
-	tester = NULL; // tester
+	tester = NULL;
 	if (tester != NULL)
 		return ;
-	//	ret = 0; // tester1
-	//	while (ret >= 0) // tester1
 	str[0] = '\0';
 	while (ft_strcmp_2(str, "\4", 1) || in == 1)
 	{
